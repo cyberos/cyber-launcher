975e78541c17d924041099dd94d10c190a91bc4a
omame <me@omame.tech>
2021-01-16 00:36:49 +0100

    Why didn't it include src/wallpaper.cpp?

---
+++ src/wallpaper.cpp
@@ -0,0 +1,42 @@
+#include "wallpaper.h"
+
+Wallpaper::Wallpaper(QObject *parent)
+    : QObject(parent)
+    , m_interface("org.cyber.Settings",
+                  "/Theme", "org.cyber.Theme",
+                  QDBusConnection::sessionBus(), this)
+{
+    if (m_interface.isValid()) {
+        connect(&m_interface, SIGNAL(wallpaperChanged(QString)), this, SLOT(onWallpaperChanged(QString)));
+        connect(&m_interface, SIGNAL(darkModeDimsWallpaerChanged()), this, SIGNAL(dimsWallpaperChanged()));
+
+        m_wallpaper = m_interface.property("wallpaper").toString();
+        emit wallpaperChanged();
+    }
+}
+
+QString Wallpaper::wallpaper() const
+{
+    return m_wallpaper;
+}
+
+bool Wallpaper::dimsWallpaper() const
+{
+    return m_interface.property("darkModeDimsWallpaer").toBool();
+}
+
+void Wallpaper::launch(const QString &command, const QStringList &args)
+{
+    QProcess process;
+    process.setProgram(command);
+    process.setArguments(args);
+    process.startDetached();
+}
+
+void Wallpaper::onWallpaperChanged(QString path)
+{
+    if (path != m_wallpaper) {
+        m_wallpaper = path;
+        emit wallpaperChanged();
+    }
+}
