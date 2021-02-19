b688438f1eebf37bb5643303dee4d8228a5a47e4
omame <me@omame.tech>
2021-02-19 10:30:27 +0000

    Translated using Weblate (English (United States))
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CyberOS/Launcher
    Translate-URL: https://hosted.weblate.org/projects/cyberos/launcher/en_US/

--- translations/en_US.ts
+++ translations/en_US.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="en_US">
 <context>
     <name>Launcher</name>
     <message>
         <location filename="../src/launcher.cpp" line="44"/>
         <source>Launcher</source>
-        <translation type="unfinished"></translation>
+        <translation>Launcher</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../qml/LauncherGridDelegate.qml" line="46"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Open</translation>
     </message>
 </context>
 <context>
@@ -22,12 +22,12 @@
     <message>
         <location filename="../qml/main.qml" line="81"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Search</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="114"/>
         <source>Not found</source>
-        <translation type="unfinished"></translation>
+        <translation>Not found</translation>
     </message>
 </context>
 </TS>
