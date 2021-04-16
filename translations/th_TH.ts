a8590205ac7fcef25d72a5106f1a37ecd5c7f6a5
Anonymous <noreply@weblate.org>
2021-04-16 06:12:48 +0000

    Translated using Weblate (Thai)
    Currently translated at 0.0% (0 of 4 strings)
    
    Translation: CyberOS/Launcher
    Translate-URL: https://hosted.weblate.org/projects/cyberos/launcher/th/

--- translations/th_TH.ts
+++ translations/th_TH.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../src/launcher.cpp" line="44"/>
         <source>Launcher</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">เปิด</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../qml/LauncherGridDelegate.qml" line="46"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">เปิด</translation>
     </message>
 </context>
 <context>
@@ -22,12 +22,12 @@
     <message>
         <location filename="../qml/main.qml" line="81"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ค้นหา</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="114"/>
         <source>Not found</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ไม่พบ</translation>
     </message>
 </context>
 </TS>
