c700702eb8769802dbcdd5bfffc68f535d563c4b
Anonymous <noreply@weblate.org>
2021-05-05 08:31:43 +0000

    Translated using Weblate (Lithuanian)
    Currently translated at 0.0% (0 of 4 strings)
    
    Translation: CyberOS/Launcher
    Translate-URL: https://hosted.weblate.org/projects/cyberos/launcher/lt/

--- translations/lt_LT.ts
+++ translations/lt_LT.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../src/launcher.cpp" line="44"/>
         <source>Launcher</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Paleidimo įrenginys</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../qml/LauncherGridDelegate.qml" line="46"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Atviras</translation>
     </message>
 </context>
 <context>
@@ -22,12 +22,12 @@
     <message>
         <location filename="../qml/main.qml" line="81"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Paieška</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="114"/>
         <source>Not found</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Nerastas</translation>
     </message>
 </context>
 </TS>
