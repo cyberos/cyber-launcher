efd8a135102e589e29eca52364f8c2721bfc64ce
Leap of Azzam <sayyid.azzam.mufhadol@gmail.com>
2021-05-06 03:12:17 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CyberOS/Launcher
    Translate-URL: https://hosted.weblate.org/projects/cyberos/launcher/id/

--- translations/id_ID.ts
+++ translations/id_ID.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../src/launcher.cpp" line="44"/>
         <source>Launcher</source>
-        <translation type="unfinished">Peluncur</translation>
+        <translation>Peluncur</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../qml/LauncherGridDelegate.qml" line="46"/>
         <source>Open</source>
-        <translation type="unfinished">Buka</translation>
+        <translation>Buka</translation>
     </message>
 </context>
 <context>
@@ -22,12 +22,12 @@
     <message>
         <location filename="../qml/main.qml" line="81"/>
         <source>Search</source>
-        <translation type="unfinished">Cari</translation>
+        <translation>Cari</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="114"/>
         <source>Not found</source>
-        <translation type="unfinished">Tidak ditemukan</translation>
+        <translation>Tidak ditemukan</translation>
     </message>
 </context>
 </TS>
