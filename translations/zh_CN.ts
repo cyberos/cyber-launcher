13dd1ce50ca00759eb76ccaab4f9a525d5ad2f83
omame <me@omame.tech>
2021-05-12 13:49:48 +0200

    Fix the translation mess

--- translations/zh_CN.ts
+++ translations/zh_CN.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_HK" sourcelanguage="zh_TW">
+<TS version="2.1" language="zh_CN" sourcelanguage="de_DE">
 <context>
     <name>Launcher</name>
     <message>
         <location filename="../src/launcher.cpp" line="44"/>
         <source>Launcher</source>
-        <translation>程式啟動器</translation>
+        <translation>应用启动器</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../qml/LauncherGridDelegate.qml" line="46"/>
         <source>Open</source>
-        <translation>開啟</translation>
+        <translation>打开</translation>
     </message>
 </context>
 <context>
@@ -22,12 +22,12 @@
     <message>
         <location filename="../qml/main.qml" line="81"/>
         <source>Search</source>
-        <translation>搵程式</translation>
+        <translation>搜索</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="114"/>
         <source>Not found</source>
-        <translation>乜都搵唔到</translation>
+        <translation>未找到</translation>
     </message>
 </context>
 </TS>
