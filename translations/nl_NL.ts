4cfcfb747cc2ce305cb709432428fcc8dfc497e5
maxius65 <max.dewispelaere@hotmail.com>
2021-05-15 19:53:06 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CyberOS/Launcher
    Translate-URL: https://hosted.weblate.org/projects/cyberos/launcher/nl/

--- translations/nl_NL.ts
+++ translations/nl_NL.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../src/launcher.cpp" line="44"/>
         <source>Launcher</source>
-        <translation type="unfinished">Launcher</translation>
+        <translation>Launcher</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../qml/LauncherGridDelegate.qml" line="46"/>
         <source>Open</source>
-        <translation type="unfinished">Open</translation>
+        <translation>Open</translation>
     </message>
 </context>
 <context>
@@ -22,12 +22,12 @@
     <message>
         <location filename="../qml/main.qml" line="81"/>
         <source>Search</source>
-        <translation type="unfinished">Zoeken</translation>
+        <translation>Zoeken</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="114"/>
         <source>Not found</source>
-        <translation type="unfinished">Niet gevonden</translation>
+        <translation>Niet gevonden</translation>
     </message>
 </context>
 </TS>
