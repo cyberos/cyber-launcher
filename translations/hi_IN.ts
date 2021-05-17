e770a99658830aefcc26e9096454dbbdec934c91
SilvinSooraj <silvanosnelli@gmail.com>
2021-05-17 19:27:55 +0000

    Translated using Weblate (Hindi)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CyberOS/Launcher
    Translate-URL: https://hosted.weblate.org/projects/cyberos/launcher/hi/

--- translations/hi_IN.ts
+++ translations/hi_IN.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../src/launcher.cpp" line="44"/>
         <source>Launcher</source>
-        <translation type="unfinished">लांचर</translation>
+        <translation>लांचर</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../qml/LauncherGridDelegate.qml" line="46"/>
         <source>Open</source>
-        <translation type="unfinished">खुला हुआ</translation>
+        <translation>खुलो</translation>
     </message>
 </context>
 <context>
@@ -22,12 +22,12 @@
     <message>
         <location filename="../qml/main.qml" line="81"/>
         <source>Search</source>
-        <translation type="unfinished">खोज</translation>
+        <translation>खोज</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="114"/>
         <source>Not found</source>
-        <translation type="unfinished">नहीं मिला</translation>
+        <translation>नहीं मिला</translation>
     </message>
 </context>
 </TS>
