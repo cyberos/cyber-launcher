ffdcd7445810dfcb38817ab38e9fafec9101d086
Anonymous <noreply@weblate.org>
2021-05-18 22:25:50 +0000

    Translated using Weblate (Yue (yue_HK))
    Currently translated at 0.0% (0 of 4 strings)
    
    Translation: CyberOS/Launcher
    Translate-URL: https://hosted.weblate.org/projects/cyberos/launcher/yue_HK/

--- translations/yue_HK.ts
+++ translations/yue_HK.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../src/launcher.cpp" line="44"/>
         <source>Launcher</source>
-        <translation>程式啟動器</translation>
+        <translation type="unfinished">程式啟動器</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../qml/LauncherGridDelegate.qml" line="46"/>
         <source>Open</source>
-        <translation>開啟</translation>
+        <translation type="unfinished">開啟</translation>
     </message>
 </context>
 <context>
@@ -22,12 +22,12 @@
     <message>
         <location filename="../qml/main.qml" line="81"/>
         <source>Search</source>
-        <translation>搵程式</translation>
+        <translation type="unfinished">搵程式</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="114"/>
         <source>Not found</source>
-        <translation>乜都搵唔到</translation>
+        <translation type="unfinished">乜都搵唔到</translation>
     </message>
 </context>
 </TS>
